fee4efbb68061266116564bfd97455efc226d69f
matthewpoe <matthewpoe@gmail.com>
2025-10-19 15:29:04 -0400

    Update web_interface.py

--- web_interface.py
+++ web_interface.py
@@ -1036,4 +1036,5 @@ if __name__ == '__main__':
     print("Press Ctrl+C to stop the server.")
     print("="*70 + "\n")
     
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
